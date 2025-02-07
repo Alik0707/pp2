fa7a0e76d4e4a9d92801a6c65e3125cb88da1cef
Alikhan <zanybek00@mail.ru>
2025-02-07 15:43:49 +0500

    3lab

--- check.py
+++ check.py
@@ -1,4 +1,17 @@
-import math
-x = int(10)
-y = int(3)
-print(100/30)
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def info(self):
+        return f"Я животное по имени {self.name}"
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        self.name = name  # Вызываем конструктор родителя
+        self.breed = breed      # Дополнительный атрибут
+
+    def info(self):  # Переопределяем метод
+        return f"{super().info()} и я породы {self.breed}"
+
+dog = Dog("Шарик", "Овчарка")
+print(dog.info())  # Я животное по имени Шарик и я породы Овчарка
