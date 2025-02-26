2973e3f4ab7e76196bccb771a633a083c23bf77f
Alikhan <zanybek00@mail.ru>
2025-02-26 18:08:31 +0500

    lab6

--- check.py
+++ check.py
@@ -1,17 +1,2 @@
-class Animal:
-    def __init__(self, name):
-        self.name = name
-
-    def info(self):
-        return f"Я животное по имени {self.name}"
-
-class Dog(Animal):
-    def __init__(self, name, breed):
-        self.name = name  # Вызываем конструктор родителя
-        self.breed = breed      # Дополнительный атрибут
-
-    def info(self):  # Переопределяем метод
-        return f"{super().info()} и я породы {self.breed}"
-
-dog = Dog("Шарик", "Овчарка")
-print(dog.info())  # Я животное по имени Шарик и я породы Овчарка
+import os 
+os.mkdir("sdff")
