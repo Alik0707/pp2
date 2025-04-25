546c475702e58165eb6e0b81dc38df81f8a4219a
Alik0707 <a_zhanybekov@kbtu.kz>
2025-04-25 18:54:40 +0500

    lab11last

--- lab.11/insert _or_update.sql
+++ lab.11/insert _or_update.sql
@@ -4,13 +4,13 @@ AS $$
 DECLARE 
     existing_id INT;
 BEGIN
-    -- Поиск пользователя с таким именем и фамилией
+    -- Поиск пользователя с таким именем и фамилией и добавляем его в эту переменную 
     SELECT user_id INTO existing_id 
     FROM phonebook 
     WHERE phonebook.name = insert_or_update_user.name AND phonebook.surname = insert_or_update_user.surname;
 
     -- Если нашли пользователя  that обновляем
-    IF FOUND THEN
+    IF FOUND THEN--found работает только так
         UPDATE phonebook 
         SET phone = insert_or_update_user.phone 
         WHERE user_id = existing_id;
